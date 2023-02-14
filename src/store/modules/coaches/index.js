63d20eaad8c8e5aadf00469492d78e26502dbb8f
marwal <mariusz.waldoch98@gmail.com>
2023-02-14 19:18:08 +0100

    Small update

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -10,12 +10,12 @@ export default {
       coaches: [
         {
           id: 'c1',
-          firstName: 'Maximilian',
-          lastName: 'Schwarzmüller',
+          firstName: 'Bill',
+          lastName: 'Gates',
           areas: ['frontend', 'backend', 'career'],
           description:
-            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-          hourlyRate: 30
+            "I'm Bill and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30,
         },
         {
           id: 'c2',
@@ -24,12 +24,12 @@ export default {
           areas: ['frontend', 'career'],
           description:
             'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
-          hourlyRate: 30
-        }
-      ]
+          hourlyRate: 30,
+        },
+      ],
     };
   },
   mutations,
   actions,
-  getters
+  getters,
 };
